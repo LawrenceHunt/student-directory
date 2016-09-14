35cb34ae9108e4d8d2a3d46a8275be1a27c9ed13
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-09-14 12:59:30 +0100

    Additional commit to be sure.

--- directory.rb
+++ directory.rb
@@ -5,10 +5,10 @@ end
 
 
 def input_students
-  puts "Please enter a student name."
-  puts "To finish, just hit return twice"
+  puts "Please enter a student name.".center(60)
+  puts "To finish, just hit return twice".center(60)
 
-  #create an empty array
+#create an empty array
 students = []
 
 #get the first name
@@ -17,18 +17,34 @@ name = gets.chomp
 #while the name is not empty, repeat this code
 while !name.empty? do
 
-puts "Hobby?"
+puts "Cohort?".center(60)
+cohort = gets.chomp
+if cohort.empty?
+  cohort = "November"
+end
+
+
+puts "Hobby?".center(60)
 hobby = gets.chomp
+if hobby.empty?
+  hobby = "Baseball"
+end
 
-puts "Country of Origin?"
+puts "Country of Origin?".center(60)
 country = gets.chomp
+if country.empty?
+  country = "England"
+end
 
-puts "Height?"
+puts "Height?".center(60)
 height = gets.chomp
+if height.empty?
+  height = 160
+end
 
 
 #add the student hash to the array
-students << {name: name, cohort: :november, hobby: hobby, country: country, height: height}
+students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
 puts "Now we have #{students.count} students. Now enter another name, or hit enter twice to end."
 name = gets.chomp
 end
@@ -37,7 +53,10 @@ end
 
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.length == 1
+  puts "Overall, we have #{students.count} great student.".center(60)
+else puts "Overall, we have #{students.count} great students.".center(60)
+end
 end
 
 
@@ -45,7 +64,7 @@ def print(students)
   n = 0
   while n <= students.length
     if(students[n][:name][0].downcase == "a" && students[n][:name].length <= 12)
-    puts "#{n+1}. #{students[n][:name]} (#{students[n][:cohort]} cohort). Hobby: #{students[n][:hobby]}. Country: #{students[n][:country]}. Height: #{students[n][:height]}."
+    puts "#{n+1}. #{students[n][:name]} (#{students[n][:cohort]} cohort). Hobby: #{students[n][:hobby]}. Country: #{students[n][:country]}. Height: #{students[n][:height]}.".center(60)
     n += 1
     end
   end
